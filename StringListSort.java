15f7b1cffff4681d9999f5fdae5a0785a00e6171
AtleySt <atleystvrain@gmail.com>
2023-04-05 12:12:16 -0500

    complete

--- StringListSort.java
+++ StringListSort.java
@@ -1,12 +1,40 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
-public class StringListSearch {
+public class StringListSort {
 
-    public static int unorderedSearch(ArrayList<String> items, String term) {
-        return 0;
+    public static ArrayList<Integer> selectionSort(ArrayList<Integer> nums) {
+        for (int i = 0; i < nums.size(); i++) {
+            int minIndex = i;
+            int minVal = nums.get(i);
+            for (int k = i+1; k < nums.size(); k++) {
+                if (nums.get(k) < minVal) {
+                    minVal = nums.get(k);
+                    minIndex = k;
+                }
+            }
+            int ph = nums.get(i);
+            nums.set(i, nums.get(minIndex));
+            nums.set(minIndex, ph);
+
+        }
+        return nums;
+    }
+
+    public static ArrayList<Integer> insertionSort(ArrayList<Integer> nums) {
+        for (int i = 1; i < nums.size(); i++) {
+            int k = i;
+
+            while (i > 0 && nums.get(i-1) > nums.get(i)) {
+                int ph = nums.get(k-1);
+                nums.set(k-1,nums.get(k));
+                nums.set(k, ph);
+                k--;
+            }
+        }
+        return nums;
     }
     
     public static void main(String[] args) {
-        
     }
 }
--- StringListSortTest.java
+++ StringListSortTest.java
@@ -8,6 +8,27 @@ import org.junit.Test;
 public class StringListSortTest {
     @Test
     public void testSelectionSort() {
+        ArrayList<Integer> inputNums;
+        ArrayList<Integer> expectedOutput;
+        ArrayList<Integer> actualOutput;
+
         
+        inputNums = new ArrayList<>(Arrays.asList(5,1,2,7,9,0,6));
+        expectedOutput = new ArrayList<>(Arrays.asList(0,1,2,5,6,7,9));;
+        actualOutput = StringListSort.selectionSort(inputNums);
+        assertEquals(expectedOutput, actualOutput);
+    }
+
+    @Test
+    public void testInsertionSort() {
+        ArrayList<Integer> inputNums;
+        ArrayList<Integer> expectedOutput;
+        ArrayList<Integer> actualOutput;
+
+        
+        inputNums = new ArrayList<>(Arrays.asList(5,1,2,7,9,0,6));
+        expectedOutput = new ArrayList<>(Arrays.asList(0,1,2,5,6,7,9));;
+        actualOutput = StringListSort.selectionSort(inputNums);
+        assertEquals(expectedOutput, actualOutput);
     }
 }
